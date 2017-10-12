6622949e4830928698f8f6efb3f06f246908bc1d
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-10-12 10:41:09 -0500

    add employees to database.

--- update_exercise.sql
+++ update_exercise.sql
@@ -1,12 +1,11 @@
 USE codeup_test_db;
 
-
 SELECT * FROM albums;
 
 UPDATE albums
 SET sales = (sales * 10);
 
-SELECT name FROM albums WHERE release_data < 1980;
+SELECT name FROM albums WHERE release_date < 1980;
 
 UPDATE albums
 SET release_date = 1980;
---
+++ where_exercise.sql
@@ -0,0 +1,9 @@
+USE employees;
+
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+
